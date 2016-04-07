f6adfb6a50bab4b7ce7130b03a8156401e561986
luis <luisinweb@gmail.com>
2016-04-07 14:37:34 -0300

    criado projeto inicial com yo

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dfDashboard', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
